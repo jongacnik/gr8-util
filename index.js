282ea28251c5e2eeaab21739dbb64e32258b2234
Jon Gacnik <jon@folderstudio.com>
2017-07-13 10:31:00 -0700

    Better ruleset composition

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var flatten = require('arr-flatten')
+var assert = require('assert')
+var objectValues = require('object-values')
+
+/**
+ * Todo
+ *
+ * - [ ] Multiple props: ['margin-left', 'margin-right']
+ * - [ ] Simple declaration
+ * - [ ] After option
+ */
+
 module.exports = util
 
-var test = {
+var simple = {
   prop: [
     'margin',
     'margin-top',
@@ -17,36 +28,130 @@ var test = {
   unit: 'rem'
 }
 
+var medium = {
+  prop: {
+    mgn: 'margin',
+    mgnt: 'margin-top',
+    mgnr: 'margin-right',
+    mgnb: 'margin-bottom',
+    mgnl: 'margin-left'
+  },
+  vals: {
+    n: 0,
+    s: 1,
+    b: 2
+  },
+  transform: i => i * 2,
+  unit: 'rem',
+  join: '-'
+}
+
 function util (opts) {
-  var styles = opts.prop.map(function (p) {
-    return opts.vals.map(function (v) {
-      return ruleset({
-        classname: classname(abbreviate(p), v),
-        property: p,
-        value: value(v, opts.unit)
-      })
+  return getRulesets({
+    prefixes: getPrefixes(opts.prop),
+    properties: getProperties(opts.prop),
+    suffixes: getSuffixes(opts.vals),
+    values: getValues(opts.vals, opts.transform),
+    join: opts.join,
+    unit: opts.unit
+  })
+}
+
+function getPrefixes (input) {
+  var prefixes
+
+  if (typeof input === 'string') {
+    prefixes = [ abbreviate(input) ]
+  } else if (Array.isArray(input)) {
+    prefixes = input.map(i => abbreviate(i))
+  } else if (typeof input === 'object') {
+    prefixes = Object.keys(input)
+  }
+
+  assert.ok(prefixes, 'gr8-util: opts.props should be a string, array, or object')
+
+  return prefixes
+}
+
+function getProperties (input) {
+  var properties
+
+  if (typeof input === 'string') {
+    properties = [ input ]
+  } else if (Array.isArray(input)) {
+    properties = input
+  } else if (typeof input === 'object') {
+    properties = objectValues(input)
+  }
+
+  assert.ok(properties, 'gr8-util: opts.props should be a string, array, or object')
+
+  return properties
+}
+
+function getSuffixes (input) {
+  var suffixes
+
+  if (typeof input === 'string' || isFinite(String(input))) {
+    suffixes = [ input ]
+  } else if (Array.isArray(input)) {
+    suffixes = input
+  } else if (typeof input === 'object') {
+    suffixes = Object.keys(input)
+  }
+
+  assert.ok(suffixes, 'gr8-util: opts.vals should be a string, number, array, or object')
+
+  return suffixes.map(i => dedecimal(i))
+}
+
+function getValues (input, transform = i => i) {
+  var values
+
+  if (typeof input === 'string' || isFinite(String(input))) {
+    values = [ input ]
+  } else if (Array.isArray(input)) {
+    values = input
+  } else if (typeof input === 'object') {
+    values = objectValues(input)
+  }
+
+  assert.ok(values, 'gr8-util: opts.vals should be a string, number, array, or object')
+
+  return values.map(i => transform(i))
+}
+
+function getRulesets (opts) {
+  var rulesets = opts.prefixes.map(function (prefix, i) {
+    return opts.values.map(function (value, j) {
+      return ruleset(
+        classname(prefix, opts.suffixes[j], opts.join),
+        declaration(opts.properties[i], value, opts.unit)
+      )
     })
   })
 
-  return flatten(styles).join("\n")
+  return flatten(rulesets).join("\n")
 }
 
-function abbreviate (str) {
-  return str.split('-').map(function (word) {
-    return word[0]
-  }).join('')
+function dedecimal (input) {
+  return String(input).replace('.', '-')
+}
+
+function abbreviate (input) {
+  return String(input).split('-').map(word => word[0]).join('')
 }
 
 function classname (prefix, suffix, joinwith = '') {
   return `${prefix}${joinwith}${suffix}`
 }
 
-function value (val, unit) {
-  return `${val}${val && unit || ''}`
+function declaration (property, value, unit) {
+  return `${property}:${value}${value && unit || ''}`
 }
 
-function ruleset (classname, property, value) {
-  return `.${classname}{${property}:${value}}`
+function ruleset (classname, declaration) {
+  return `.${classname}{${declaration}}`
 }
 
-console.log(gr8(test))
+console.log(util(simple))
